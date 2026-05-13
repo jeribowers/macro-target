2ffd57f5305643cc9798877444c0634af92285a8
Jeri Bowers <7104347+jeribowers@users.noreply.github.com>
2026-05-13 19:49:52 +0200

    update supabase details

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const SUPABASE_URL = 'https://cdobxcexgwomqxryhzqw.supabase.co';
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNkb2J4Y2V4Z3dvbXF4cnloenF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzNDI3NTIsImV4cCI6MjA4ODkxODc1Mn0.Cv81mlP-Xg7_zXCgyso_BIDAc_KibzLZulRElWZmcME';
+export const SUPABASE_URL = 'https://crhpnplqqwfgkmmulvuz.supabase.co';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNyaHBucGxxcXdmZ2ttbXVsdnV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxNTgzNjcsImV4cCI6MjA5MTczNDM2N30.iEVgvrta2IUAjhbxjGXRJqhhBvprTGYnC_2Bt09VAfM';
